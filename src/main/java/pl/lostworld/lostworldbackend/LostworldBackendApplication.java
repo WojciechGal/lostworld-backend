ff1c0e4e2b794fc07ac5e8f357db1302642a159b
Wojciech <wojciech.gladalski@gmail.com>
2020-03-21 09:25:06 +0100

    first commit

---
+++ src/main/java/pl/lostworld/lostworldbackend/LostworldBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.lostworld.lostworldbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostworldBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostworldBackendApplication.class, args);
+    }
+
+}
