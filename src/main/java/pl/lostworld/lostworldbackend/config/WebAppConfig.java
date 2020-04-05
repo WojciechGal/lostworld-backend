c7409677030b672b74a69a5ca195ae51568c9b2d
Wojciech <wojciech.gladalski@gmail.com>
2020-04-05 14:11:29 +0200

    garbage destroyed

--- src/main/java/pl/lostworld/lostworldbackend/config/WebAppConfig.java
+++ src/main/java/pl/lostworld/lostworldbackend/config/WebAppConfig.java
@@ -2,7 +2,6 @@ package pl.lostworld.lostworldbackend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,12 +11,5 @@ public class WebAppConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/403").setViewName("test/403");
-//        registry.addViewController("/").setViewName("test/home");
-//        registry.addViewController("/login").setViewName("test/login");
     }
 }
