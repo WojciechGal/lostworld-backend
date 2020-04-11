bb716dbf9e9ab9b4125aac725f9d73f3ea08dc93
Wojciech <wojciech.gladalski@gmail.com>
2020-04-11 14:58:18 +0200

    zmiana adresow ratingow

--- src/main/java/pl/lostworld/lostworldbackend/rating/country/CountryRatingController.java
+++ src/main/java/pl/lostworld/lostworldbackend/rating/country/CountryRatingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/countriesRatings")
+@RequestMapping("/ratings/countries")
 public class CountryRatingController {
 
     private CountryRatingService countryRatingService;
