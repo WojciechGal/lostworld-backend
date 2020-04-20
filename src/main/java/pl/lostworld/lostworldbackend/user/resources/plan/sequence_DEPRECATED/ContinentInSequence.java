0df5102ba85749f48c29bc71e16e44dd63e7cab5
Wojciech <wojciech.gladalski@gmail.com>
2020-04-20 20:19:46 +0200

    wylaczenie klasy deprecated

--- src/main/java/pl/lostworld/lostworldbackend/user/resources/plan/sequence_DEPRECATED/ContinentInSequence.java
+++ src/main/java/pl/lostworld/lostworldbackend/user/resources/plan/sequence_DEPRECATED/ContinentInSequence.java
@@ -1,29 +1,22 @@
 package pl.lostworld.lostworldbackend.user.resources.plan.sequence_DEPRECATED;
 
-import lombok.Getter;
-import lombok.Setter;
-import pl.lostworld.lostworldbackend.continent.Continent;
-import pl.lostworld.lostworldbackend.user.resources.plan.TravelPlan;
-
-import javax.persistence.*;
-
-@Entity
-@Getter
-@Setter
-@Table(name = "travel_plan_continent_in_sequence")
+//@Entity
+//@Getter
+//@Setter
+//@Table(name = "travel_plan_continent_in_sequence")
 public class ContinentInSequence {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private int positionInPlan;
-
-    @ManyToOne
-    private TravelPlan travelPlan;
-
-    @ManyToOne
-    private Continent continent;
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private int positionInPlan;
+//
+//    @ManyToOne
+//    private TravelPlan travelPlan;
+//
+//    @ManyToOne
+//    private Continent continent;
 
     //po stronie TravelPlan
 //    @OneToMany(mappedBy = "travelPlan", cascade = CascadeType.ALL)
