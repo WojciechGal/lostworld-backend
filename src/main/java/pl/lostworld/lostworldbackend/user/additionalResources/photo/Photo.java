d5d785b9e1c311c949947ff02b7ad895f6271516
Wojciech <wojciech.gladalski@gmail.com>
2020-05-24 13:01:05 +0200

    dodanie encji po stronie zdjecia

--- src/main/java/pl/lostworld/lostworldbackend/user/additionalResources/photo/Photo.java
+++ src/main/java/pl/lostworld/lostworldbackend/user/additionalResources/photo/Photo.java
@@ -3,8 +3,10 @@ package pl.lostworld.lostworldbackend.user.additionalResources.photo;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import pl.lostworld.lostworldbackend.templates.DBFile;
+import pl.lostworld.lostworldbackend.user.additionalResources.album.Album;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -34,6 +36,8 @@ public class Photo extends DBFile {
     @NotNull
     private String fileType;
 
-    //todo po testach, gdy front będzie gotowy trzeba dodać encję użytkownika
+    @ManyToOne
+    @NotNull
+    private Album album;
 }
 
