3a22794bec6f6784fb0ce5b91094a7c4b76bfc3d
Wojciech <wojciech.gladalski@gmail.com>
2020-06-06 14:25:27 +0200

    uzależnienie encji od template'u

--- src/main/java/pl/lostworld/lostworldbackend/user/additionalResources/plan/TravelPlan.java
+++ src/main/java/pl/lostworld/lostworldbackend/user/additionalResources/plan/TravelPlan.java
@@ -1,61 +1,19 @@
 package pl.lostworld.lostworldbackend.user.additionalResources.plan;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-import pl.lostworld.lostworldbackend.city.City;
-import pl.lostworld.lostworldbackend.continent.Continent;
-import pl.lostworld.lostworldbackend.country.Country;
-import pl.lostworld.lostworldbackend.relic.Relic;
-import pl.lostworld.lostworldbackend.user.User;
+import pl.lostworld.lostworldbackend.templates.UserResource;
 import pl.lostworld.lostworldbackend.user.additionalResources.report.Report;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Getter
 @Setter
 @Table(name = "travel_plans")
-public class TravelPlan {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne
-    @NotNull
-    private User user;
-
-    @CreationTimestamp
-    private LocalDateTime createDateTime;
-
-    @UpdateTimestamp
-    private LocalDateTime updateDateTime;
-
-    @ManyToMany
-    @JoinTable(inverseJoinColumns = @JoinColumn(name = "continent_in_sequence_id"))
-    private List<Continent> sequenceOfContinents = new ArrayList<>();
-
-    @ManyToMany
-    @JoinTable(inverseJoinColumns = @JoinColumn(name = "country_in_sequence_id"))
-    private List<Country> sequenceOfCountries = new ArrayList<>();
-
-    @ManyToMany
-    @JoinTable(inverseJoinColumns = @JoinColumn(name = "city_in_sequence_id"))
-    private List<City> sequenceOfCities = new ArrayList<>();
-
-    @ManyToMany
-    @JoinTable(inverseJoinColumns = @JoinColumn(name = "relic_in_sequence_id"))
-    private List<Relic> sequenceOfRelics = new ArrayList<>();
-
-    private String title;
-
-    private String description;
+public class TravelPlan extends UserResource {
 
     @OneToOne(mappedBy = "travelPlan")
     private Report report;
