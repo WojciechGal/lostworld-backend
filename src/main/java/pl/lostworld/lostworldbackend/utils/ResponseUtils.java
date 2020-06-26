6276a6f11e1ab72d4165214c5b230386460d6014
Wojciech <wojciech.gladalski@gmail.com>
2020-06-26 16:14:59 +0200

    klasa utils do response'ow

---
+++ src/main/java/pl/lostworld/lostworldbackend/utils/ResponseUtils.java
@@ -0,0 +1,36 @@
+package pl.lostworld.lostworldbackend.utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.*;
+
+public class ResponseUtils {
+
+    public static <T> ResponseEntity<?> designCreatedResponse(T createdObject) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", HttpStatus.CREATED.value());
+        body.put("object", createdObject);
+
+        return new ResponseEntity<>(body, HttpStatus.CREATED);
+    }
+
+    public static <T> ResponseEntity<?> designAcceptedResponse(T acceptedObject) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", HttpStatus.ACCEPTED.value());
+        body.put("object", acceptedObject);
+
+        return new ResponseEntity<>(body, HttpStatus.ACCEPTED);
+    }
+
+    public static <T> ResponseEntity<?> designOkResponse(T response) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", HttpStatus.OK.value());
+        body.put("object", response);
+
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    }
+}
