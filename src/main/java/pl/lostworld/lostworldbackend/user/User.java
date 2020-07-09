a639cc28f4aa5556226a2f552383d3a66d0c99b6
Wojciech <wojciech.gladalski@gmail.com>
2020-07-09 17:53:36 +0200

    dodanie konstruktora pod ID

--- src/main/java/pl/lostworld/lostworldbackend/user/User.java
+++ src/main/java/pl/lostworld/lostworldbackend/user/User.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -32,8 +33,13 @@ import java.util.Set;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @Table(name = "users")
 public class User {
+
+    public User(Long id) {
+        this.id = id;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
